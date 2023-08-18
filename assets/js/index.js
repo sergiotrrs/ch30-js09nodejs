9d31ab736f678aadfc7c283dd82f034d3e5dda8a
Sergio Torres <sergio.torres@idr.edu.mx>
2023-08-18 16:47:41 -0600

    ci: uso de axios

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,18 @@
 console.log("Sesión js09 NPM y Axios");
+
+const apiFetch = document.getElementById("apiFetch");
+const apiAxios = document.getElementById("apiAxios");
+
+apiFetch.onclick = async () => {
+    const responseJson = await fetch("https://fakestoreapi.com/products");
+    const response = await responseJson.json();    
+    console.log( response );
+}
+
+/**
+ * Solicitud HTTP usando Axios
+ */
+apiAxios.onclick = async () => {
+    const response = await axios.get("https://fakestoreapi.com/products");    
+    console.log( response );
+}
